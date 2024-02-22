a189590907e74ca716bb5cc494ef9d3e033ee528
prayogisholihul <prayogisholihul@gmail.com>
2024-02-22 20:49:40 +0700

    rupiah amount

--- lib/expense_item.dart
+++ lib/expense_item.dart
@@ -32,7 +32,7 @@ class _ExpenseItemState extends State<ExpenseItem> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  '\$${widget.expenseData.amount.toStringAsFixed(2)}',
+                  '\Rp. ${widget.expenseData.amount.toStringAsFixed(2)}',
                   style: TextStyle(color: Colors.black),
                 ),
                 Container(
